0c61e34a0cb6cc7ff915c913528839a4d029c0ef
choongmanee <choongmanee@users.noreply.github.com>
2020-06-26 06:29:32 +0000

    Automated deployment: Fri Jun 26 06:29:32 UTC 2020 427d11abf79b7934049cb5242ef8687361a22c07

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "b3901eefca887494dd7f788be191599a",
+  "main.dart.js": "aa91fb862436a0c641bb4c858b43eefd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "3d7209732f667ecd56a138793e79aade",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
